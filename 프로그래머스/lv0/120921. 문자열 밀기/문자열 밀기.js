8872f2aa7a0820da7582d810e984c2efbb9e7bab
tjsdnTm <77582425+alwlsTm@users.noreply.github.com>
2023-10-04 23:49:42 +0900

    [level 0] Title: 문자열 밀기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.js
@@ -0,0 +1,10 @@
+function solution(A, B) {
+    let arr = [...A];
+    
+    for (let i = 0; i < A.length; i++) {
+        if (arr.join("") === B) return i;
+        arr.unshift(arr.pop());
+    }
+    
+    if (arr.join("") !== B) return -1;
+}

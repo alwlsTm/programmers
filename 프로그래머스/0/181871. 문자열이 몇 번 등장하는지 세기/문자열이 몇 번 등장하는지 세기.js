8f931fa4f95f67630dfba07515332b33dc4509e4
bang <77582425+alwlsTm@users.noreply.github.com>
2024-03-28 20:01:38 +0900

    [level 0] Title: 문자열이 몇 번 등장하는지 세기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.js
@@ -0,0 +1,8 @@
+function solution(myString, pat) {
+    let result = 0;
+    for (let i = 0; i < myString.length; i++) {
+        let str = myString.slice(i, i + pat.length);
+        if (pat === str) result ++;
+    }
+    return result;
+}

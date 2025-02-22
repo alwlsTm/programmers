aa8680ed407f6a8ea3137f055bb0b84ff04aa2cf
bang <77582425+alwlsTm@users.noreply.github.com>
2025-02-22 22:15:51 +0900

    [level 2] Title: 올바른 괄호, Time: 27.20 ms, Memory: 36.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,8 @@
+function solution(s){
+    let cnt = 0;
+    for (let i = 0; i < s.length; i++) {
+        cnt += (s[i] === '(') ? 1 : -1;
+        if (cnt < 0) break;
+    }
+    return cnt === 0 ? true : false;
+}
